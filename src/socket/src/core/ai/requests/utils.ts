8217a675b79dfc2c2796dd21869a47c42657bf37
dynafer <dynaferkim@gmail.com>
2025-08-31 17:48:29 +0100

    fix: fixed wrong default flows url env var

--- src/socket/src/core/ai/requests/utils.ts
+++ src/socket/src/core/ai/requests/utils.ts
@@ -3,8 +3,12 @@ import BaseRequest from "@/core/ai/requests/BaseRequest";
 import LangflowRequest from "@/core/ai/requests/Langflow";
 import { EBotPlatform, EBotPlatformRunningType } from "@/models/bot.related.types";
 import InternalBot from "@/models/InternalBot";
+import { Utils } from "@langboard/core/utils";
 
 export const createRequest = (internalBot: InternalBot): BaseRequest | null => {
+    internalBot.platform = Utils.String.convertSafeEnum(EBotPlatform, internalBot.platform);
+    internalBot.platform_running_type = Utils.String.convertSafeEnum(EBotPlatformRunningType, internalBot.platform_running_type);
+
     switch (internalBot.platform) {
         case EBotPlatform.Default:
             switch (internalBot.platform_running_type) {
